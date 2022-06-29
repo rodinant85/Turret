6e4bc24e4f7260e6126a2baf2b0e815e2bdb5c25
rodinant85 <rodinant85@gmail.com>
2022-06-29 13:17:17 +0300

    added machine_gun build script

---
+++ install_machine_gun.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+git clone --branch machine_gun https://github.com/rodinant85/Turret
+
+cd Turret
+cd rear_sight
+mkdir build;cd build
+cmake ..
+make -j4
+cp rear_sight ../rear_sight
+cd ..
+rm -rf build
+cd ..
+cp -r rear_sight ../rear_sight
+
+echo "install python library and main program..."
+
+sudo sh ./install_python_lib.sh
+sudo sh ./install_main.sh
+
+echo "install services..."
+sudo cp *.service /etc/systemd/system
+sudo systemctl daemon-reload
+
+sudo systemctl enable machine_gun_main.service
+
+cd ..
+rm -rf Turret/
+
+echo "reboot..."
+sudo reboot
+
