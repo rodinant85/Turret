4838bc85319d03f9e7e37856c8599a9e95739a75
rodinant85 <rodinant85@gmail.com>
2022-06-29 14:14:53 +0300

    added install service

---
+++ install_motor_control.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+git clone --branch motor_control_plate https://github.com/rodinant85/Turret
+cd Turret
+
+echo "install python library and main program..."
+
+sudo sh ./install_python_lib.sh
+sudo sh ./install_main_and_websocket.sh
+
+echo "install services..."
+sudo cp machine_* /etc/systemd/system
+sudo systemctl daemon-reload
+
+sudo systemctl enable machine_gun_motor_control_main.service
+
+cd ..
+rm -rf Turret/
+
+echo "reboot..."
+sudo reboot
