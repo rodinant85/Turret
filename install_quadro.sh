ce1ed835a6cf250ead680983c517c796c022c988
rodinant85 <rodinant85@gmail.com>
2022-06-29 11:05:30 +0300

    added scripts

--- install_quadro.sh
+++ install_quadro.sh
@@ -2,6 +2,7 @@
 
 git clone --branch quadro https://github.com/rodinant85/Turret
 
+cd Turret
 cd rear_sight
 mkdir build;cd build
 cmake ..
@@ -9,19 +10,24 @@ make -j4
 cp rear_sight ../rear_sight
 cd ..
 rm -rf build
+cd ..
 cp -r rear_sight ../rear_sight
 
-./install_python_lib.sh
-./install_main_and_websocket.sh
+echo "install python library and main program..."
 
+sudo sh ./install_python_lib.sh
+sudo sh ./install_main_and_websocket.sh
 
+echo "install services..."
 sudo cp quadro_* /etc/systemd/system
 sudo systemctl daemon-reload
 
 sudo systemctl enable quadro_main.service
 
-rm -rf *
+cd ..
+rm -rf Turret/
 
+echo "reboot..."
 sudo reboot
 
 
